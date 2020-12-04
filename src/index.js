eac52fd01fc7e8ebb1b432dbbd328ba5d0a1c220
FAKER-A <1037881273@qq.com>
2020-12-04 15:56:37 +0800

    feat: 更改项目目录

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import debounce from "./debounce" 
+import getType from "./getType"
+import deduplication from "./deduplication"
+import flat from "./flat"
+export {
+  debounce,
+  getType,
+  deduplication,
+  flat
+}

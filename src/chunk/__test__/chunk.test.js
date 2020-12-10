cf01aa3c1bec9682ddf03f3ff7e2f31f3c5d27df
FAKER-A <1037881273@qq.com>
2020-12-10 14:30:26 +0800

    feat(chunk): 添加chunk函数

---
+++ src/chunk/__test__/chunk.test.js
@@ -0,0 +1,25 @@
+import chunk from '../index';
+
+describe('chunk', () => {
+  test('不穿入参数', () => {
+    expect(chunk()).toEqual([]);
+  });
+  test('传入空数组', () => {
+    expect(chunk([])).toEqual([]);
+  });
+  test('传入非数组', () => {
+    expect(chunk(1)).toBe(1);
+  });
+  test('传入正常数组', () => {
+    expect(chunk([1, 2, 3])).toEqual([[1], [2], [3]]);
+  });
+  test('传入size大于数组的的长度', () => {
+    expect(chunk([1, 2, 3], 4)).toEqual([1, 2, 3]);
+  });
+  test('传入size为0', () => {
+    expect(chunk([1, 2, 3], 0)).toEqual([1, 2, 3]);
+  });
+  test('传入size不能被数组的长度整除', () => {
+    expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import debounce from './debounce';
 import getType from './getType';
 import deduplication from './deduplication';
 import flat from './flat';
+import chunk from './chunk';
 
 export {
   debounce,
   getType,
   deduplication,
   flat,
+  chunk,
 };
